4908ae014f2005d9bdbd483aeedf1470c7668c07
Amal Abeygunawardana <amalabey@gmail.com>
2020-05-28 07:52:17 +1000

    Add basic a gui window frame class

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import wx
 
-app = wx.App()
-frame = wx.Frame(parent=None, title='Hello World')
-frame.Show()
-app.MainLoop()
+class MyFrame(wx.Frame):    
+    def __init__(self):
+        super().__init__(parent=None, title='Hello World')
+        panel = wx.Panel(self)
+
+        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
+        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55))
+
+        self.Show()
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
